416aceecf50b5172e8700977333b0056980308a6
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-11-07 12:05:39 -0300

    feat: add collision detection

--- main.cpp
+++ main.cpp
@@ -2,15 +2,31 @@
 
 const float g_worldLimit = 100.0f;
 
+float g_worldLimitX = g_worldLimit;
+float g_worldLimitY = g_worldLimit;
+
 float g_posX = 0.0f;
 float g_posY = 0.0f;
 float g_squareSize = 20.0f;
 
-float g_velocityX = 0.02f;
-float g_velocityY = 0.015f;
+float g_velocityX = 0.1f;
+float g_velocityY = 0.15f;
 
-void display()
-{
+inline float getHalfSize() {
+    return g_squareSize / 2.0f;
+}
+
+void checkCollision(float* pos, float* velocity, float limit, float halfSize) {
+    if (*pos + halfSize > limit) {
+        *velocity = -*velocity;
+        *pos = limit - halfSize;
+    } else if (*pos - halfSize < -limit) {
+        *velocity = -*velocity;
+        *pos = -limit + halfSize;
+    }
+}
+
+void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
@@ -37,27 +53,20 @@ void display()
     glutSwapBuffers();
 }
 
-void update()
-{
+void update() {
     g_posX += g_velocityX;
     g_posY += g_velocityY;
 
 
+    float halfSize = getHalfSize();
+
+    checkCollision(&g_posX, &g_velocityX, g_worldLimitX, halfSize);
+    checkCollision(&g_posY, &g_velocityY, g_worldLimitY, halfSize);
+
     glutPostRedisplay();
 }
 
-void initialize()
-{
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-100.0f, 100.0f, -100.0f, 100.0f);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-void resize(int width, int height)
-{
+void resize(int width, int height) {
     if (height == 0) height = 1;
 
     glViewport(0, 0, width, height);
@@ -70,17 +79,31 @@ void resize(int width, int height)
     if (width >= height)
     {
         gluOrtho2D((-g_worldLimit * aspect), (g_worldLimit * aspect), -g_worldLimit, g_worldLimit);
+        g_worldLimitX = g_worldLimit * aspect;
+        g_worldLimitY = g_worldLimit;
     }
     else
     {
         gluOrtho2D(-g_worldLimit, g_worldLimit, (-g_worldLimit / aspect), (g_worldLimit / aspect));
+        g_worldLimitX = g_worldLimit;
+        g_worldLimitY = g_worldLimit / aspect;
     }
 
     glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int argc, char** argv)
-{
+void initialize() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    int width = glutGet(GLUT_WINDOW_WIDTH);
+    int height = glutGet(GLUT_WINDOW_HEIGHT);
+    if (width <= 0) width = 1024;
+    if (height <= 0) height = 768;
+
+    resize(width, height);
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
 
 
