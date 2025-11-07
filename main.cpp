96b7ff3a7c3e6b2ae5b878641eda68dd3005d6c3
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-11-07 10:46:39 -0300

    feat: add orthographic projection and window resize handling for OpenGL scene

--- main.cpp
+++ main.cpp
@@ -1,14 +1,52 @@
 #include <GL/glut.h>
 
+const float g_worldLimit = 100.0f;
+
+float g_posX = 0.0f;
+float g_posY = 0.0f;
+float g_squareSize = 20.0f;
+
 void draw()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+
+    glColor3f(0.9f, 0.5f, 0.6f);
+    glRectf(-10.0f, -10.0f, 10.0f, 10.0f);
+
+    glutSwapBuffers();
 }
 
 void initialize()
 {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-100.0f, 100.0f, -100.0f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void resize(int width, int height)
+{
+    if (height == 0) height = 1;
+
+    glViewport(0, 0, width, height);
+
+    float aspect = (float) width / (float) height;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    if (width >= height)
+    {
+        gluOrtho2D((-g_worldLimit * aspect), (g_worldLimit * aspect), -g_worldLimit, g_worldLimit);
+    }
+    else
+    {
+        gluOrtho2D(-g_worldLimit, g_worldLimit, (-g_worldLimit / aspect), (g_worldLimit / aspect));
+    }
+
+    glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv)
@@ -19,10 +57,12 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("DVD Screensaver MVP");
+    glutCreateWindow("DVD Screensaver");
 
 
     glutDisplayFunc(draw);
+    glutReshapeFunc(resize);
+
     initialize();
 
     glutMainLoop();
